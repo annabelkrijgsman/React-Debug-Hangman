f1ab6568c59b9fe9c699fac9e8aeeb97ab1fea3a
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-08 13:30:06 +0100

    Added bug fix explanation

--- HangmanReact-exercise-3/src/components/App/App.js
+++ HangmanReact-exercise-3/src/components/App/App.js
@@ -11,13 +11,11 @@ import WronglyGuessedLetters from "../WronglyGuessedLetters/WronglyGuessedLetter
 
 const wordGuessed = (word, guessedLetters) => {
   word = word.split("");
-  // remove all letters from word that are already guessed
-  // We can do this with a for loop to.
   let remaining = word.filter(letter =>
-    // If the letter is guessed return false (we want to remove that then)
-    guessedLetters.includes(letter)
+    // BUG FIX
+    // ! ontbrak vóór guessedLetters.includes(letter)
+    !guessedLetters.includes(letter)
   );
-  // If we have letters left the word is not yet guessed
   return remaining.length === 0;
 };
 
