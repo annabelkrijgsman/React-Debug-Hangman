b679e8865e83e523df69207740c9b541eacadb1b
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-08 12:58:53 +0100

    Initial commit

---
+++ HangmanReact-exercise-2/src/components/App/App.js
@@ -0,0 +1,62 @@
+// This is basically a layout component
+
+// BUG FIX
+// Ik heb eerst in App.js gecheckt of wordGuessed een true of false returned. Deze returned een true.
+// Vervolgens heb ik in GameOver.js gecheckt of props.wordGuessed true of false was. Deze was undefined.
+// Daarna heb ik props ge-concole.logged hierin ontdekte ik de typo wordGuesed (met 1 s) in de component GameOver in App.js:
+// <GameOver chosenWord={game.chosenWord} wordGuesed={wordWasGuessed} />
+
+// Deze heb ik aangepast naar:
+// <GameOver chosenWord={game.chosenWord} wordGuessed={wordWasGuessed} />
+
+import React from "react";
+import "./App.css";
+import ChosenWord from "../ChosenWord/ChosenWord";
+import TextInput from "../TextInput/TextInput";
+import GameOver from "../GameOver/GameOver";
+import ResetGameButton from "../ResetGameButton/ResetGameButton";
+import GuessesLeft from "../GuessesLeft/GuessesLeft";
+import WronglyGuessedLetters from "../WronglyGuessedLetters/WronglyGuessedLetters";
+
+const wordGuessed = (word, guessedLetters) => {
+  word = word.split("");
+  const remaining = word.filter(letter => !guessedLetters.includes(letter));
+  return remaining.length === 0;
+};
+
+const getWrongLetters = (word, guessedLetters) =>
+  guessedLetters.filter(letter => !word.split("").includes(letter));
+
+const isGameOver = (game, wrongLetters, wordWasGuessed) =>
+  wordWasGuessed || wrongLetters.length >= game.maxGuesses;
+
+const App = props => {
+  const game = props.game;
+  const wordWasGuessed = wordGuessed(game.chosenWord, game.guessedLetters);
+  const wrongLetters = getWrongLetters(game.chosenWord, game.guessedLetters);
+  const gameIsOver = isGameOver(game, wrongLetters, wordWasGuessed);
+
+  const gameOver = gameIsOver ? (
+    <GameOver chosenWord={game.chosenWord} wordGuessed={wordWasGuessed} />
+  ) : null;
+
+  return (
+    <div className="App">
+      <h1>Simple 'Hangman' Game</h1>
+      <p>[no people will actually be harmed during this game]</p>
+      {gameOver}
+      <TextInput
+        currentChosenLetter={game.currentChosenLetter}
+        gameIsOver={gameIsOver}
+        change={props.chosenLetterHandler}
+        submit={props.guessLetterHandler}
+      />
+      <ChosenWord word={game.chosenWord} guessedLetters={game.guessedLetters} />
+      <ResetGameButton click={props.restartGameHandler} />
+      <GuessesLeft wrongLetters={wrongLetters} maxGuesses={game.maxGuesses} />
+      <WronglyGuessedLetters wrongLetters={wrongLetters} />
+    </div>
+  );
+};
+
+export default App;
